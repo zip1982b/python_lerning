9c0eed4df7146b7b8980aaab8a6b5fa83c5287b1
zip1982b <zip1982b@gmail.com>
2021-09-16 14:14:27 +0200

    nested lists complete

---
+++ nested_lists.py
@@ -0,0 +1,12 @@
+
+
+
+
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input))):
+        name = input()
+        score = input()
+        students.extend([[name, score]])
+
+
