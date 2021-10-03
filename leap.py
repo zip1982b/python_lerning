ff2e69f6813a4e0e1b8510ae89a0e2eec7590d9e
zip1982b <zip1982b@gmail.com>
2021-10-03 15:42:29 +0200

    leap is solved

---
+++ leap.py
@@ -0,0 +1,17 @@
+
+
+
+
+def is_leap(year):
+    leap = False
+    if not(year%4):
+        leap = True
+        if not(year%100):
+            leap = False
+            if not(year%400):
+                leap = True
+    return leap
+
+
+
+
